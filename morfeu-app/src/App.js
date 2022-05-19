1ddf54a545f722d2155c5b53f28e89d9ee06a160
Alexandre Henrique <83831769+DevAlexandre1@users.noreply.github.com>
2022-05-19 12:23:58 -0300

    Inicial project

--- morfeu-app/src/App.js
+++ morfeu-app/src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import ButtonHome from './components/ButtonHome';
+import imghome from './imgs/HomePageButton.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <div className="Container">
-        <label>
-          <input></input>
-          <input></input>          
-        </label>
-      </div>
+    
+       <div className="App">
+        <div className="container">          
+          <header><p>Header</p>          
+              <h1>home Page</h1>  
+          </header>
+          <main><p>Main</p>
+          </main>
+          <footer>            
+            <div>
+                <ButtonHome/>
+                <img src></img>
+            </div>          
+          </footer>
+        </div>      
     </div>
+    
   );
 }
 
---
+++ morfeu-app/src/components/ButtonHome.js
@@ -0,0 +1,12 @@
+const ButtonHome = () => {
+  return (
+    <div>
+        <img src="../src/imgs/HomePageButton.jpg" alt="home"></img>
+        <h1 className="home">
+            HOME
+        </h1>
+    </div>
+  )
+}
+
+export default ButtonHome
